8ea5f5b5ab7a9f915a8a084408009b458ba5ae6d
Eduard-Ionut <pocheaeduardionut@gmail.com>
2026-05-08 19:12:15 +0300

    Etapa 4: Erori - inceput

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express= require("express");
 const path= require("path");
+const fs= require("fs"); 
+const sass= require("sass");
 
 app= express();
 app.set("view engine", "ejs")
+
+
+obGlobal= {
+    obErori: null,
+    obImagini: null,
+    folderScss: path.join(__dirname, "resurse/scss"),
+    folderCss: path.join(__dirname, "resurse/css"),
+    folderBackup: path.join(__dirname, "backup"),
+}
 
 console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
@@ -15,13 +26,34 @@ app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 //     res.sendFile(path.join(__dirname, "index.html"));
 // });
 
-app.get("/", function (req, res){
+app.get(["/", "/index", "/home"], function (req, res){
     //res.sendFile(path.join(__dirname, "index.html"));
     res.render("pagini/index");
 });
 
 app.get("/despre", function (req, res){
     res.render("pagini/despre");
+});
+
+
+function initErori(){
+    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
+    let  erori= obGlobal.obErori= JSON.parse(continut)
+    let err_default= erori.eroare_default
+    err_default.imagine= path.join(erori.cale_baza, err_default.imagine)
+    for (let eroare of erori.info_erori){
+        eroare.imagine= path.join(erori.cale_baza, eroare.imagine)
+    }
+}
+initErori()
+
+app.get("/eroare", function (req, res){
+    res.render("pagini/eroare",
+        {
+            imagine: obGlobal.obErori.eroare_default.imagine,
+            titlu: obGlobal.obErori.eroare_default.titlu,
+            text: obGlobal.obErori.eroare_default.text,
+        });
 });
 
 
